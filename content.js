a2d09e06650b83f9e44ceca4b001e21c5bd5d056
brennanho <brennanmho@gmail.com>
2019-01-02 00:55:03 -0800

    Version 1.0.2

---
+++ content.js
@@ -0,0 +1,74 @@
+//Author: Brennan Ho
+//Date: 12/11/2018
+//Content.js is used to modify page contents on the current tab (i.e. seekTo video time)
+
+//Helper Functions
+function get_timestamp(tag) {
+	return tag.attributes.item(0).nodeValue;
+}
+
+function get_caption(tag) {
+	return tag.textContent.toLowerCase().replace(/<\/?[^>]+(>|$)/g, "").replace(/\s+/g,' ').trim();
+}
+
+//https://stackoverflow.com/questions/18740664/search-whole-word-in-string
+function is_match(search_on_string, search_text) {
+  search_text = search_text.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+  return search_on_string.match(new RegExp("\\b"+search_text+"\\b", "i")) != null;
+}
+
+function populate_captions(response) {
+	let tags = Array.prototype.slice.call(response.getElementsByTagName('text'));
+	let captions = tags.map(get_caption);
+	let timestamps = tags.map(get_timestamp);
+
+	let captions_and_timestamps = {};
+	for (var i = 0; i < captions.length; i++) {
+		captions_and_timestamps[captions[i]] = timestamps[i];
+	}
+	return captions_and_timestamps;
+}
+//End of Helper Functions
+
+var captions_and_timestamps = {};
+var Http = new XMLHttpRequest();
+
+//Used for downloading, searching captions, and seeking to a time in a video
+//Message passing occurs between content.js and popup.js here
+chrome.runtime.onMessage.addListener(function(response, sender, send_response) {
+	//Populates captions_and_timestamps: called only when youtube video cc is turned on
+	if (response.captions != undefined) {
+		Http.onreadystatechange = function() {
+			if (this.readyState == 4) {
+				captions_and_timestamps = populate_captions(Http.responseXML);
+			}
+		}
+		Http.open("GET", response.captions);
+		Http.send();
+		send_response({status: "Captions received"});	
+	}
+	
+	//Populates list of timestamps when user inputs a word/phrase
+	else if (response.user_search != undefined) {
+		var search_results = [];
+		var phrase_results = [];
+		for (var caption in captions_and_timestamps) {
+			if (is_match(caption, response.user_search)) {
+				search_results.push(captions_and_timestamps[caption]);
+				let caption_split = caption.split(response.user_search);
+				let colored_search = '<font color="red">' + response.user_search + ' </font>'
+				let colored_caption = caption_split[0] + colored_search + caption_split[1];
+				phrase_results.push(colored_caption);
+			}
+		}
+		send_response({timestamps: search_results, phrases: phrase_results});
+	}
+
+	//Goes to vid_time in youtube video when user clicks on the button
+	else if (response.vid_time != undefined) {
+		var player = document.getElementsByTagName("video")[0];
+		player.currentTime = parseFloat(response.vid_time);
+		console.log(response.vid_time);
+	}
+
+});
---
+++ popup.js
@@ -0,0 +1,146 @@
+//Author: Brennan Ho
+//Date: 12/11/2018
+//Popup.js is used as the 'main' interface for searching captions and selecting times to go seek to in the video
+
+//Helper Functions
+//Converts seconds to 'HH:MM:SS'
+function sec_to_hms(secs){
+	 var hours = parseInt(Math.floor(secs/3600));
+	 secs %= 3600;
+	 var mins = parseInt(Math.floor(secs/60));
+	 secs = parseInt(secs % 60);
+	 if (hours < 10)
+	 	hours = "0" + hours;
+	 if (mins < 10)
+	 	mins = "0" + mins;
+	 if (secs < 10)
+	 	secs = "0" + secs;
+
+	 if (hours == "00")
+	 	return mins + ":" + secs;
+
+	 return hours + ":" + mins + ":" + secs;
+}
+
+//Add timestamp buttons (search results) to popup
+function add_timestamps(div, tab_id, timestamps, phrases) {
+	for (let i = 0; i < timestamps.length; i++) {
+		let button = document.createElement("button");
+		button.setAttribute("id", "timestamp");
+		let timestamp = sec_to_hms(parseInt(timestamps[i]));
+		let timestamp_secs = timestamps[i];
+		button.innerHTML = timestamp.bold() + ": " + phrases[i];
+
+		//User has selected a timestamp button
+		button.addEventListener('click', function(){
+			chrome.tabs.sendMessage(tab_id, {vid_time: timestamp_secs}, function(response_click){
+				console.log("Sending timestamp_secs to content script:", timestamp_secs);
+			});
+		});
+		div.appendChild(button);
+	}
+}
+//End of Helper Functions
+
+//Browser Events (Chrome API)
+//Message passing occurs between content.js and popup.js here
+//Ensure extension is disabled on all tabs by default
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	for (var tab in tabs) {
+		chrome.browserAction.setIcon({path: "images/disabled-video-player.png", tabId: tab.id});
+    	chrome.browserAction.disable(tab.id);
+    }
+    //Get cached timestamps and phrases from previous search
+    try {
+	    chrome.storage.sync.get(['time_keys', 'phrases'], function(result) {
+	    	var div = document.getElementById("timestamps");
+	    	add_timestamps(div, tabs[0].id, result.time_keys, result.phrases);
+	    });
+	}
+	catch (err) {
+		console.log(err);
+	}
+});
+
+//Disable tab if not youtube video and captions are not enabled
+chrome.tabs.onUpdated.addListener(function(tab_id, change_info, tab) {
+	if (change_info.url == undefined)
+		return true;
+	chrome.storage.sync.clear(function() {
+        console.log('clearing storage of popup timestamps on url change');
+    });
+   chrome.browserAction.setIcon({path: "images/disabled-video-player.png", tabId: tab_id});
+   chrome.browserAction.disable(tab_id);
+});
+
+//Clear storage for popup contents when tab focus changes
+chrome.tabs.onActivated.addListener(function(active_info) {
+	chrome.storage.sync.clear(function() {
+        console.log('clearing storage of popup timestamps');
+    });
+});
+
+//Gets captions_url from network_log then sends over to content script for downloading + processing
+chrome.webRequest.onCompleted.addListener(function(details) {
+	if (details.url == undefined || details.initiator != "https://www.youtube.com")
+		return;
+	var captions_url = details.url.replace("fmt=srv3", "fmt=srv1");
+	
+	//Send the captions URL to the content script for parsing
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    	chrome.tabs.sendMessage(tabs[0].id, {captions: captions_url}, function(response){
+    		if (response != undefined && response.status == "Captions received") {
+	    		chrome.browserAction.enable(tabs[0].id);
+	    		chrome.browserAction.setIcon({path: "images/video-player.png", tabId: tabs[0].id});
+	    	}
+	    });
+	});
+
+}, {urls: ["https://www.youtube.com/api/timedtext?*"]});
+
+//User search and input
+function user_input(input) {
+	if (input.value.length == 0)
+		return;
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	    chrome.tabs.sendMessage(tabs[0].id, {user_search: input.value}, function(response){
+	    	var timestamps = response.timestamps;
+	    	var phrases = response.phrases;
+	    	var div = document.getElementById("timestamps");
+
+	    	//Remove timestamp buttons (search results) from popup
+	    	while (div.childElementCount > 2) {
+				div.removeChild(div.lastChild);
+			}
+
+			//Display message for no search results found
+			if (timestamps.length == 0) {
+				let failed_message = document.createElement("h4");
+				failed_message.innerHTML = "No search results found";
+				div.appendChild(failed_message);
+			}
+			else {
+			//Add current search results to display
+		    	add_timestamps(div, tabs[0].id, timestamps, phrases);
+		    	chrome.storage.sync.set({time_keys: timestamps, phrases: phrases}, function() {
+          			console.log('Storing current popup.html contents');
+        		});
+		   }
+
+	    });
+	});
+}
+//End of Browser Events (Chrome API)
+
+//Main App Buttons
+//Search button click
+document.getElementById('search-button').addEventListener('click', function() { 
+user_input(document.getElementById('phrase-box'));
+});
+//Enter key pressed
+document.getElementById('phrase-box').addEventListener('keyup', function(event) {
+event.preventDefault();
+if (event.keyCode === 13) 
+    user_input(document.getElementById('phrase-box'));
+});
